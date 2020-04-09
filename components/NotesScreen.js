2cd81e2df87d558776ab0c2f0181f44d04a736b2
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-04-09 14:31:08 +0100

    notes layout worked on

--- components/NotesScreen.js
+++ components/NotesScreen.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
 
 export const NotesScreen = ({ navigation }) => {
-    const [titleVal, settitleVal] = React.useState('');
+    const [titleVal, settitleVal, note, setnote] = React.useState('');
 
     return (
         <View>
             <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                style={{ height: 60}}
+                placeholder="ADD TITLE..."
                 onChangeText={settitleVal}
                 value={titleVal}
             />
+            <TextInput underlineColorAndroid="transparent"
+                placeholder="Add Description..."
+                value = {note}
+                multiline={true} />
         </View>
     );
 }
